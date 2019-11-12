225783eea6070a20882f6103e90a5c202dcb34fc
hoelzlm <markus.hoelzle@multivac.de>
2019-11-12 14:14:29 +0100

    implemented update and delete for label route

--- src/routes/label.py
+++ src/routes/label.py
@@ -26,3 +26,26 @@ def insert():
     db.connection.commit()
 
     return jsonify({"message": "ok"})
+
+
+@label_route.route('/label/<_id>', methods=['PUT'])
+def update(_id):
+    data = request.get_json()
+
+    with db.connection.cursor() as cursor:
+        sql = "UPDATE label SET text=%s, color=%s WHERE id=%s"
+        cursor.execute(sql, (data['text'], data['color'], _id))
+    db.connection.commit()
+
+    return jsonify(db.get_one_by_id("label", _id))
+
+
+@label_route.route('/label/<_id>', methods=['DELETE'])
+def delete(_id):
+
+    with db.connection.cursor() as cursor:
+        sql = "DELETE FROM label WHERE id=%s"
+        cursor.execute(sql, (_id, ))
+    db.connection.commit()
+
+    return jsonify({"message": "ok"})
