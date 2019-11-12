773a61f6ecd077a82c90dbd544fbda123f8a71d0
hoelzlm <markus.hoelzle@multivac.de>
2019-11-12 09:27:29 +0100

    some more init

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""Main File to run application"""
+
+
+class ScrapTicketSystem:
+    def __init__(self):
+        print("hello world")
+
+
+if __name__ == "__main__":
+    ScrapTicketSystem()
---
+++ src/database/__init__.py
@@ -0,0 +1,8 @@
+"""database connection and handling"""
+
+import pymysql
+
+
+class Database:
+    def __init__(self):
+        pass
