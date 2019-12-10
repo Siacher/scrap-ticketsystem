acddd0600897bdc59e2dffea9ff4de7d5e66de25
hoelzlm <markus.hoelzle@multivac.de>
2019-12-10 09:36:00 +0100

    adding role based permissions

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -62,6 +62,10 @@ class Database:
             # create table label_in_ticket
             cursor.execute("""CREATE TABLE IF NOT EXISTS label_in_tabel_(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ticket INT, label INT, FOREIGN KEY (ticket) REFERENCES ticket(id), FOREIGN KEY (label) REFERENCES label(id))""")
 
+
+            # create roles
+            #cursor.execute("""INSERT INTO user_group (name) VALUES ('admin'), ('default'), ('processor')""")
+
         self.connection.commit()
 
     def get_all(self, table):
--- src/routes/api/user.py
+++ src/routes/api/user.py
@@ -33,9 +33,24 @@ def insert():
     with db.connection.cursor() as cursor:
         sql = "INSERT INTO user(email, password, first_name, last_name) VALUES (%s, %s, %s, %s)"
         cursor.execute(sql, (data['email'], generate_hash(data['password']), data['first_name'], data['last_name']))
+
+        sql = "SELECT * FROM user WHERE email=%s"
+        cursor.execute(sql, (data['email'],))
+
+        result = cursor.fetchone()
+
+        print(data['password'])
+        print(result['password'])
+        print(check_hash(data['password'], result['password']))
+
+        if not check_hash(data['password'], result['password']):
+            return jsonify({'message': 'mail or password invalid'})
+        else:
+            token = Auth.generate_token(result['id'])
+
     db.connection.commit()
 
-    return jsonify({"message": "ok"})
+    return jsonify({'token': token})
 
 
 @user_route.route('/user/<_id>', methods=['DELETE'])
@@ -90,3 +105,44 @@ def login():
 def get_user_by_token():
     _id =g.user['id']
     return jsonify(db.get_one_by_id("user", _id))
+
+
+@user_route.route('/user/me/can_change/<t_id>', methods=['GET'])
+@Auth.auth_required
+def can_change(t_id):
+    _id =g.user['id']
+
+    with db.connection.cursor() as cursor:
+        sql = 'SELECT ug.name FROM user as u JOIN user_in_group as uig on u.id = uig.user_id JOIN user_group as ug on uig.group_id = ug.id WHERE u.id=%s'
+        cursor.execute(sql, (_id, ))
+        result = cursor.fetchall()
+
+        ticket = db.get_one_by_id('ticket', t_id)
+
+        value = False
+        for item in result:
+            if item['name'] == "admin":
+                value = True
+
+            if item['name'] == "processor" and ticket['assign_to'] == _id:
+                value = True
+
+        return jsonify({"message": str(value).lower()})
+
+
+@user_route.route('/user/me/is_admin', methods=['GET'])
+@Auth.auth_required
+def is_admin():
+    _id =g.user['id']
+
+    with db.connection.cursor() as cursor:
+        sql = 'SELECT ug.name FROM user as u JOIN user_in_group as uig on u.id = uig.user_id JOIN user_group as ug on uig.group_id = ug.id WHERE u.id=%s'
+        cursor.execute(sql, (_id, ))
+        result = cursor.fetchall()
+
+        value = False
+        for item in result:
+            if item['name'] == "admin":
+                value = True
+
+        return jsonify({"message": str(value).lower()})
