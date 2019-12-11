16ccc65e4eae1d2359f61baa875968aef229f7a8
hoelzlm <markus.hoelzle@multivac.de>
2019-12-11 12:22:03 +0100

    forms

--- src/forms/__init__.py
+++ src/forms/__init__.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
-    passwort = StringField('passwort', validators=[DataRequired()])
-    remember_me = BooleanField('eingeloggt bleiben', validators=[DataRequired()])
-    submit = SubmitField('Login', validators=[DataRequired()])
+    passwort = PasswordField('passwort', validators=[DataRequired()])
+    remember_me = BooleanField('eingeloggt bleiben')
+    submit = SubmitField('Login')
+
+
+class CreateTicketForm(FlaskForm):
+    prios = []
+    categorys = []
+
+    header =  StringField('Überschrift')
+    category = SelectField('Kategorie', choices=[(1, "eins"), (2, "zwei")])
+    prio = SelectField('Priorität', choices=[(1, "eins"), (2, "zwei")])
+    text = TextAreaField('Text')
+    submit = SubmitField('Anlegen')
--- src/routes/public/index.py
+++ src/routes/public/index.py
@@ -5,7 +5,7 @@ from . import LUser
 from src.shared.authentification import Auth
 import bcrypt
 
-from src.forms import LoginForm
+from src.forms import LoginForm, CreateTicketForm
 
 index_route = Blueprint('index', __name__)
 
@@ -64,7 +64,7 @@ def login():
 @index_route.route('/logout', methods=['GET'])
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('index.login'))
 
 
 @index_route.route('/register', methods=['GET'])
@@ -74,12 +74,18 @@ def register():
 
 @index_route.route('/ticket/<_id>', methods=['GET'])
 def ticket(_id):
-    return render_template('ticket.html')
+    with db.connection.cursor() as cursor:
+        sql = "SELECT t.header as header, t.text as text, p.text as prio, c.text as category FROM ticket as t LEFT JOIN prio as p on t.prio_id = p.id LEFT JOIN category as c on t.category_id = c.id WHERE t.id = %s"
+        cursor.execute(sql, (_id,))
+        result = cursor.fetchone()
+
+    return render_template('ticket.html', ticket=result)
 
 
 @index_route.route('/create_ticket', methods=['GET'])
 def create_ticket():
-    return render_template('create_ticket.html')
+    form = CreateTicketForm()
+    return render_template('create_ticket.html', form=form)
 
 
 @index_route.route('/create_category', methods=['GET'])
